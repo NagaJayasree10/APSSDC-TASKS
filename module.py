7550a03d3ca8b076ea2d161de48dcc13b6c1c6c1
NagaJayasree10 <62808498+NagaJayasree10@users.noreply.github.com>
2021-06-16 13:04:45 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,17 @@
+# Day=9_16-06-2021
+import camelcase
+import emoji
+import pyttsx3
+def conn_camelcase():
+    a=input("Enter your data: ")
+    x=camelcase.CamelCase()
+    if a==x.hump(a):
+        print(emoji.emojize(":thumbs_up:"))
+    else:
+        print("The converted data is: ")
+        x=camelcase.CamelCase()
+        print(x.hump(a))
+        spk=pyttsx3.init()
+        spk.say("Successfully converted")
+        spk.runAndWait()
+    
